99f5f021f7d15081cc4cdc50f74e8c7cfe9643ab
yeashin007 <yeashin007@gmail.com>
2024-02-19 00:54:35 +0600

    Coplete functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,12 @@ const btnApply = document.getElementById("btn-apply");
 const cuponInputEl = document.getElementById("cupon-input");
 const discountElement = document.getElementById("discount-element");
 const grandTotalEl = document.getElementById("grand-total");
+const couponContainer = document.getElementById("coupon-container");
+const btnNext = document.getElementById("btn-next");
+const inputPhone = document.getElementById("phone");
 
 // declared variable
-let bookedSeat = 0;
+let bookedSeat = parseInt(seatCounter.innerText);
 let totalPrice = 0;
 let grandTotal = 0;
 let remainSeat = parseInt(remainSeatEl.innerText);
@@ -56,6 +59,12 @@ for (const seat of allSeats) {
       // update total ticket price
       totolPriceEl.innerText = totalPrice;
 
+      // enabled coupon input and button
+      if (bookedSeat === 4) {
+        cuponInputEl.removeAttribute("disabled", "");
+        btnApply.removeAttribute("disabled", "");
+      }
+
       // Grand total
       const discount = parseFloat(discountElement.innerText);
       grandTotal = totalPrice - discount;
@@ -63,6 +72,9 @@ for (const seat of allSeats) {
       grandTotalEl.innerText = grandTotal;
       seat.classList.add("bg-green");
       seat.setAttribute("disabled", "");
+
+      // enabled next btn
+      activeBtn();
 
       if (bookedSeat % 2 === 0) {
         list.style.backgroundColor = "#e7e7e7";
@@ -81,8 +93,10 @@ function calcDiscount() {
 
   if (cupon === "NEW15") {
     discount = totalPrice * 0.15;
+    couponContainer.classList.add("hidden");
   } else if (cupon === "Couple 20") {
     discount = totalPrice * 0.2;
+    couponContainer.classList.add("hidden");
   } else {
     alert("Insert valid cupon");
     return;
@@ -91,4 +105,28 @@ function calcDiscount() {
   grandTotal = totalPrice - discount;
   grandTotalEl.innerText = grandTotal;
   discountElement.parentNode.parentNode.classList.remove("hidden");
+}
+
+inputPhone.addEventListener("keyup", activeBtn);
+
+function activeBtn() {
+  if (inputPhone.value > 0 && bookedSeat > 0) {
+    btnNext.removeAttribute("disabled", "");
+  }
+}
+
+function primary(el, value) {
+  el.innerText = value;
+}
+
+function init() {
+  btnNext.setAttribute("disabled", "");
+  primary(remainSeatEl, 40);
+  primary(seatCounter, 0);
+  primary(totolPriceEl, 0);
+  primary(grandTotalEl, 0);
+
+  while (selectionList.childNodes > 0) {
+    selectionList.removeChild(selectionList.firstChild);
+  }
 }
